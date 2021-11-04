7ef4f58d26ef539bc03e6cbdadb9ed61ef5ddddd
vanquiche <stevevang2010@hotmail.com>
2021-11-04 20:33:55 +0900

    add template form for adding items

---
+++ src/app/item-form/item-form.component.ts
@@ -0,0 +1,53 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+import { Item } from '../Item';
+
+@Component({
+  selector: 'item-form',
+  templateUrl: './item-form.component.html',
+  styleUrls: ['./item-form.component.css'],
+})
+export class ItemFormComponent {
+  // TODOS
+  // create form validation
+  // require fields
+  // 'price' must be number
+
+  @Output() sendItem = new EventEmitter<Item>();
+
+  // Properties
+  // loop over for select drop-down menu
+  categorySelect = [
+    'appetizer',
+    'entree',
+    'cocktail',
+    'beverage',
+    'dessert',
+    'side',
+  ];
+  item: Item;
+
+  constructor() {
+    this.item = {
+      name: '',
+      price: 1,
+      description: '',
+      category: null,
+      listed: false,
+      happyHour: false,
+    };
+  }
+  // Methods
+
+  // updates category upon change
+  onChange(value: any) {
+    this.item.category = value;
+  }
+
+  onSubmit(data: any) {
+    // send form data to parent
+    data.form.value.id = new Date().toISOString();
+    console.log(data.form.value)
+    this.sendItem.emit(data.form.value);
+    data.reset();
+  }
+}
