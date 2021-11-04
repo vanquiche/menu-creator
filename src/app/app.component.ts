80b168e8e0e1b1d6b0f6ad9f5d71d75ca5260c30
vanquiche <stevevang2010@hotmail.com>
2021-11-04 20:34:25 +0900

    remove boiler plate and setup basic structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
 import { Component } from '@angular/core';
+import { Item } from './Item';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'angular-menu';
+  menu: Item[] = [
+    {
+      id: '001',
+      name: 'Chicken Teriyaki',
+      price: 12.79,
+      description:
+        'Marinated with a sweet glaze, grilled to perfection, served with side salad.',
+      category: 'entree',
+      listed: false,
+      happyHour: true,
+    },
+  ];
+
+  pushToMenu(item: Item) {
+    // receive form-data from child
+    this.menu.push(item);
+  }
+
+  deleteFromMenu(id: string) {
+    let index!: number;
+
+    for (const item of this.menu) {
+      if (item.id === id) {
+        index = this.menu.indexOf(item);
+      }
+    }
+
+    // console.log(index);
+    if (index > -1) {
+      this.menu.splice(index, 1);
+    } else throw new Error('could not find Item to delete');
+  }
 }
