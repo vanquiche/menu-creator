824692955a7353e2f4b1de37e0756baf95c430e8
vanquiche <stevevang2010@hotmail.com>
2021-11-05 19:07:11 +0900

    add update logic on 'happyhour' and 'listed' attr.

--- src/app/item-card/item-card.component.ts
+++ src/app/item-card/item-card.component.ts
@@ -4,23 +4,35 @@ import { Item } from '../Item';
 @Component({
   selector: 'item-card',
   templateUrl: './item-card.component.html',
-  styleUrls: ['./item-card.component.css']
+  styleUrls: ['./item-card.component.css'],
 })
 export class ItemCardComponent {
   // TODOS
-  // check box to true of false depending on item value -- done
-  // change value of checkboxes upon toggle
-  // edit and delete functions
+  // check box sets to true/false depending on item value -- done
+  // update value of checkboxes upon toggle -- done
+  // add delete logic -- done
+  // add edit logic
 
   @Input() item!: Item;
   @Output() deleteItem = new EventEmitter<string>();
-  constructor() { }
+  @Output() editItem = new EventEmitter<Item>();
+  @Output() updateItem = new EventEmitter();
+
+  constructor() {}
 
   onDelete(id: any) {
     this.deleteItem.emit(id);
   }
 
-  onEdit() {
-    
+  onEdit(item: Item) {
+    this.editItem.emit(item);
+  }
+
+  handleCheck(id?: string, selection?: string, value?: boolean) {
+    this.updateItem.emit({
+      id,
+      selection,
+      value
+    });
   }
 }
