bb8db76704221d6affa463d0bc5ca88f4e9b50c3
vanquiche <stevevang2010@hotmail.com>
2021-11-05 19:04:32 +0900

    organize code for clarity, add edit & update logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Item } from './Item';
+import { Update } from './UpdateReqest';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +8,15 @@ import { Item } from './Item';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
+  // PROPERTIES
   title = 'angular-menu';
+  // controls visibility of 'item-form'
+  openForm = false;
+  // controls visibility of 'edit-form'
+  editForm = false;
+  // container to store the item user intends to edit
+  selectedEditItem!: Item;
+
   menu: Item[] = [
     {
       id: '001',
@@ -21,23 +30,66 @@ export class AppComponent {
     },
   ];
 
-  pushToMenu(item: Item) {
+  toggleForm() {
+    this.openForm = !this.openForm;
+  }
+
+  addToMenu(item: Item) {
     // receive form-data from child
     this.menu.push(item);
   }
 
   deleteFromMenu(id: string) {
     let index!: number;
-
     for (const item of this.menu) {
       if (item.id === id) {
         index = this.menu.indexOf(item);
+        break;
       }
     }
-
-    // console.log(index);
     if (index > -1) {
       this.menu.splice(index, 1);
     } else throw new Error('could not find Item to delete');
   }
+
+  closeEditForm() {
+    this.editForm = false;
+  }
+
+  editMenuItem(item: Item) {
+    let index!: number;
+    for (const ref of this.menu) {
+      if (ref.id === item.id) {
+        index = this.menu.indexOf(ref);
+        break;
+      }
+    }
+    console.log(this.menu[index]);
+    this.selectedEditItem = item;
+    this.editForm = true;
+  }
+
+  // updates boolean value of 'listed'/'happyHour'
+  updateMenu(request: Update) {
+    // request structure:
+    // ex. {id: '001', selection: 'listed', value: true}
+
+    let index!: number;
+    for (const item of this.menu) {
+      if (item.id === request.id) {
+        index = this.menu.indexOf(item);
+        break;
+      }
+    }
+    switch (request.selection) {
+      case 'listed':
+        this.menu[index].listed = !request.value;
+        break;
+      case 'happyHour':
+        this.menu[index].happyHour = !request.value;
+        break;
+    }
+  }
+
+  // end
 }
