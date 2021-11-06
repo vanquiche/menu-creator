d6c2aaf271e53a53878aea5c3ee5041f43a6a776
vanquiche <stevevang2010@hotmail.com>
2021-11-06 18:15:19 +0900

    add ability to pass updated data to parent

--- src/app/edit-form/edit-form.component.ts
+++ src/app/edit-form/edit-form.component.ts
@@ -1,32 +1,42 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Item } from '../Item';
 import { CATEGORY_SELECT } from '../CategorySelect';
+import { Category } from '../Category';
 
 @Component({
   selector: 'edit-form',
   templateUrl: './edit-form.component.html',
-  styleUrls: ['./edit-form.component.css']
+  styleUrls: ['./edit-form.component.css'],
 })
 export class EditFormComponent {
   // TODOS
-  // drop-down menu is selected by existing value
-  // 'listed'/'happyHour' values are checked
-  // based on existing vlues
+  // drop-down menu is selected by existing value -- DONE
+  // add ngModel to each input in form -- DONE
 
   @Output() cancelEdit = new EventEmitter();
+  @Output() submitEdit = new EventEmitter<Item>();
 
   @Input() editItem!: Item;
 
   categorySelect = CATEGORY_SELECT;
+  currentCategorySelection!: Category;
 
-  constructor() { }
+  constructor() {}
 
-  onSubmit() {
-    console.log('submitted')
+  onSubmit(data: any) {
+    // console.log(data.form.value);
+    this.submitEdit.emit({
+      id: this.editItem.id,
+      category: this.currentCategorySelection,
+      ...data.form.value
+    });
   }
 
   onCancel() {
-    this.cancelEdit.emit()
+    this.cancelEdit.emit();
   }
 
+  onChange(selection: any) {
+    this.currentCategorySelection = selection;
+  }
 }
