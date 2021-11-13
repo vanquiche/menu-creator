4437a51f0404b3d124cc29c63dda28df01d246a6
vanquiche <stevevang2010@hotmail.com>
2021-11-13 15:06:21 +0900

    transform data in menu-view with pipe to return only listed items

--- src/app/menu-view/menu-view.component.ts
+++ src/app/menu-view/menu-view.component.ts
@@ -1,31 +1,33 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MenuService } from '../menu.service';
-import { Observable } from 'rxjs';
+import { Observable, from } from 'rxjs';
 import { Item } from '../Item';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'menu-view',
   templateUrl: './menu-view.component.html',
-  styleUrls: ['./menu-view.component.css']
+  styleUrls: ['./menu-view.component.css'],
 })
 export class MenuViewComponent implements OnInit {
-
   menu!: Observable<Item[]>;
   styles!: {
-    format?: string ,
-    font?: string,
-    pricing?: string
-  }
+    format?: string;
+    font?: string;
+    pricing?: string;
+  };
 
-  constructor(private menuService: MenuService) { }
+  constructor(private menuService: MenuService) {}
 
   ngOnInit() {
-    this.menu = this.menuService.menu$
+    // returns only items that are listed
+    this.menu = this.menuService.menu$.pipe(
+      map((data) => data.filter((item) => item.listed === true))
+    );
   }
 
   checkValue(val: any) {
-    this.styles = {...val}
-    console.log(this.styles)
+    this.styles = { ...val };
+    console.log(this.styles);
   }
-
 }
